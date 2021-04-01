ec7855bfe31eda5f4ce99b862dfb897bd7b7a6cd
ImanRS1 <omid-omega@mail.com>
2021-04-01 18:44:31 +0200

    added search api call, action

--- src/actions/locationAction.js
+++ src/actions/locationAction.js
@@ -1,15 +1,28 @@
-import { searchLocationURL } from "../api";
+import { searchLocationURL, defaultLocationURL } from "../api";
 import axios from "axios";
 
 export const loadLocation = () => async (dispatch) => {
-  const current = await axios.get(searchLocationURL());
-  const location = await axios.get(searchLocationURL());
+  const current = await axios.get(defaultLocationURL());
+  const location = await axios.get(defaultLocationURL());
 
   dispatch({
     type: "FETCH_LOCATION",
     payload: {
       location: location.data.location,
       current: current.data.current,
+    },
+  });
+};
+
+export const fetchSearch = (location) => async (dispatch) => {
+  const weatherCurrent = await axios.get(searchLocationURL(location));
+  const weatherLocation = await axios.get(searchLocationURL(location));
+
+  dispatch({
+    type: "FETCH_SEARCHED",
+    payload: {
+      location: weatherLocation.data.location,
+      current: weatherCurrent.data.current,
     },
   });
 };
--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 
 
-const base_url = "http://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93af2141636212003&q=london";
+const base_url = "http://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93af2141636212003&q=";
+
+/* const base_url = "http://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93af2141636212003&q=london"; */
 
 /* export const searchLocationURL = (location) => `${base_url}${location}`;
  */
 
-export const searchLocationURL = () => `${base_url}`;
+export const defaultLocationURL = () => `${base_url}stockholm`;
+export const searchLocationURL = (location) => `${base_url}${location}`;
 export const forecastLocationURL = (location) => `http://api.weatherapi.com/v1/forecast.json?key=0abe7ad125fb4e93af2141636212003&q=${location}&days=7`;
+
+
 
