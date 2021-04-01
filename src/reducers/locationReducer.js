19d70d081fe448074bcf6807c0174495cc06cc19
ImanRS1 <omid-omega@mail.com>
2021-04-01 19:19:25 +0200

    added search logic to reducer

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,14 +1,26 @@
 const initState = {
   location: [],
   current: [],
+  searched: [],
 };
 
 const locationReducer = (state = initState, action) => {
   switch (action.type) {
     case "FETCH_LOCATION":
-      return { ...state, 
+      return {
+        ...state,
         location: action.payload.location,
         current: action.payload.current,
+      };
+    case "FETCH_SEARCHED":
+      return {
+        ...state,
+        searched: action.payload.searched,
+      };
+    case "CLEAR_SEARCHED":
+      return {
+        ...state,
+        searched: [],
       };
     default:
       return { ...state };
