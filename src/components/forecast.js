c1addf5bfff91f14f88523b5b4ff39b02df586cd
ImanRS1 <omid-omega@mail.com>
2021-04-03 22:01:57 +0200

    added style to each forecast item

--- src/components/forecast.js
+++ src/components/forecast.js
@@ -7,24 +7,37 @@ const Forecast = ({ sunrise, sunset, date, weather, condition }) => {
   return (
     <div>
       <ForeCastItem>
-        <h3>
-          {date}
-          <div>Temperature: {weather} degrees celcius.</div>
-          <SunRiseAndSet>
-            <img src={sunriseImage} />
-            {sunrise}
-            <img src={sunsetImage} />
-            {sunset}
-          </SunRiseAndSet>
-          Condition: {condition.text}
-        </h3>
+        {date}
+        <div>Temperature: {weather} degrees celcius.</div>
+        <SunRiseAndSet>
+          <img src={sunriseImage} />
+          {sunrise}
+          <img src={sunsetImage} />
+          {sunset}
+        </SunRiseAndSet>
+        <Condition>
+          <img src={condition.icon} />
+          {condition.text}
+        </Condition>
       </ForeCastItem>
     </div>
   );
 };
 
+const Condition = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const ForeCastItem = styled(motion.div)`
   margin: 2rem;
+  padding: 0.5rem;
+  background-color: rgba(5, 5, 5, 0.2);
+  box-shadow: 5px 10px 18px #2b2b2b;
+  h3 {
+    margin: 0;
+  }
 `;
 
 const SunRiseAndSet = styled(motion.div)`
