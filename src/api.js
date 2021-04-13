b102d875c407fbf172602dd761f7d45a130b073f
ImanRS1 <omid-omega@mail.com>
2021-04-14 00:27:20 +0200

    modified api link

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 
 
-const base_url = "http://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93af2141636212003&q=";
+const base_url = "https://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93af2141636212003&q=";
 
 /* const base_url = "http://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93af2141636212003&q=london"; */
 
@@ -9,7 +9,7 @@ const base_url = "http://api.weatherapi.com/v1/current.json?key=0abe7ad125fb4e93
 
 export const defaultLocationURL = () => `${base_url}stockholm`;
 export const searchLocationURL = (location) => `${base_url}${location}`;
-export const forecastLocationURL = (location) => `http://api.weatherapi.com/v1/forecast.json?key=0abe7ad125fb4e93af2141636212003&q=${location}&days=7`;
+export const forecastLocationURL = (location) => `https://api.weatherapi.com/v1/forecast.json?key=0abe7ad125fb4e93af2141636212003&q=${location}&days=7`;
 
 
 
